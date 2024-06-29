cbcc036e430aae94e91b89d61dcd9a96ba8e68e1
Tiago Alves <tiago.pessoal93@gmail.com>
2024-06-29 18:02:47 -0300

    feature: request.bodyMap accept multipart/form-data

--- lib/src/extensions/request_extension.dart
+++ lib/src/extensions/request_extension.dart
@@ -1,11 +1,33 @@
 import 'dart:convert';
 
+import 'package:mime/mime.dart';
 import 'package:shelf/shelf.dart';
+import 'package:shelf_multipart/form_data.dart';
 
 extension RequestExtension on Request {
-  Future<Map<String, dynamic>?> bodyJson() async {
-    if ((contentLength ?? 0) <= 0) return null;
-    if (mimeType != 'application/json') return null;
-    return jsonDecode(await readAsString());
+  Future<Map<String, dynamic>> bodyMap() async {
+    if ((contentLength ?? 0) <= 0) return {};
+
+    if (isMultipartForm) {
+      final map = <String, dynamic>{};
+
+      await for (final form in multipartFormData) {
+        if (form.filename != null) {
+          map[form.name] = {
+            'filename': form.filename,
+            'mime_type': lookupMimeType(form.filename ?? ''),
+            'data': 'data:${lookupMimeType(form.filename ?? '') ?? 'octet/stream'};base64,${base64Encode(await form.part.readBytes())}',
+          };
+        } else {
+          map[form.name] = form.part.readString();
+        }
+      }
+
+      return map;
+    } else if (mimeType == 'application/json') {
+      return jsonDecode(await readAsString());
+    } else {
+      return {};
+    }
   }
 }
